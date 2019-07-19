bc89d52dc5a02e77094e5ca770c691494aca0911
Gavin Ha <gavinha@gmail.com>
2019-07-19 11:25:03 -0700

    convert to GRanges for PON script

--- scripts/createPanelOfNormals.R
+++ scripts/createPanelOfNormals.R
@@ -1,4 +1,5 @@
 library(HMMcopy)
+library(GenomicRanges)
 library(optparse)
 library(ichorCNA)
 
@@ -12,11 +13,12 @@ option_list <- list(
 	make_option(c("-o", "--outfile"), type = "character", help = "Output file."),
 	make_option(c("-c", "--centromere"), type="character", help = "File containing Centromere locations"),
 	make_option(c("--chrs"), type="character", default="c(1:22,\"X\")", help = "Specify chromosomes to analyze."),
+  make_option(c("--genomeStyle"), type = "character", default = "NCBI", help = "NCBI or UCSC chromosome naming convention; use UCSC if desired output is to have \"chr\" string. [Default: %default]"),
 	make_option(c("--chrNormalize"), type="character", default="c(1:22)", help = "Specify chromosomes to normalize GC/mappability biases"),
 	make_option(c("--maleChrXLogRThres"), type="numeric", default=-0.80, help = "ChrX Log ratio threshold to confirm as male gender."),
 	make_option(c("-e", "--exons.bed"), type = "character", default=NULL, help = "Path to bed file containing exon regions."),
-	make_option(c("--method"), type = "character", default="median", help="Median or Mean."),
-	make_option(c("--ylim"), type = "character", default="c(-2,2)", help="Y-limits for plotting of mean/median log ratios")
+	make_option(c("--method"), type = "character", default="median", help="Median or Mean.")
+	#make_option(c("--ylim"), type = "character", default="c(-2,2)", help="Y-limits for plotting of mean/median log ratios")
 )
 parseobj <- OptionParser(option_list=option_list)
 opt <- parse_args(parseobj)
@@ -30,35 +32,40 @@ exons.bed <- opt$exons.bed  # "0" if none specified
 centromere <- opt$centromere
 method <- opt$method
 outfile <- opt$outfile
-chrs <- eval(parse(text = opt$chrs))
-ylim <- eval(parse(text = opt$ylim))
-chrNormalize <- eval(parse(text=opt$chrNormalize))
+genomeStyle <- opt$genomeStyle
+#ylim <- eval(parse(text = opt$ylim))
 maleChrXLogRThres <- opt$maleChrXLogRThres
+chrs <- as.character(eval(parse(text = opt$chrs)))
+chrNormalize <- as.character(eval(parse(text=opt$chrNormalize))); 
+seqlevelsStyle(chrs) <- genomeStyle
+seqlevelsStyle(chrNormalize) <- genomeStyle
 
 if (!is.null(centromere)){
 	centromere <- read.delim(centromere,header=T,stringsAsFactors=F,sep="\t")
 }
+seqlevelsStyle(centromere$Chr) <- genomeStyle
 
 files <- read.delim(filelist, header = FALSE, stringsAsFactors=FALSE, sep ="\t")[, 1]
+
 normalGR <- NULL
 
 for (i in 1:length(files)){
 	### LOAD NORMAL FILES ###
 	sid <- gsub(".wig","",basename(files[i]))
 	message("Loading normal file:", files[i])
-	normal_reads <- wigToRangedData(files[i])
+	normal_reads <- wigToGRanges(files[i])
 
 	message("Reading GC and mappability files")
 	if (is.null(gcWig)) {
 	  stop("GC Wig file required")
 	} else {
-	  gc <- wigToRangedData(gcWig)
+	  gc <- wigToGRanges(gcWig)
 	}
 	if (is.null(mapWig)) {
 	  message("Normalizing without mappability Wig file.")
 	  map <- NULL
 	} else {
-	  map <- wigToRangedData(mapWig)
+	  map <- wigToGRanges(mapWig)
 	}
 		
 	## FILTER BY EXONS IF PROVIDED ##
@@ -77,21 +84,22 @@ for (i in 1:length(files)){
 	message("Correcting ", sid)
 	if (is.null(normalGR)){
 		normalGR <- normal_counts$counts
-		colnames(normalGR)[which(colnames(normalGR) %in% "copy")] <- sid
+		values(normalGR) <- values(normalGR)$copy
+		colnames(values(normalGR)) <- sid
 	}else{
-		normalGR[[sid]] <- normal_counts$counts$copy
+		values(normalGR)[[sid]] <- normal_counts$counts$copy
 	}
 	
 	chrXMedian <- gender$chrXMedian
-	chrXInd <- normalGR$space == "X"
+	chrXStr <- grep("X", chrs, value = TRUE)
+	chrXInd <- as.character(seqnames(normalGR)) == chrXStr
 	## Normalize chrX ##
-	normalGR[[sid]][chrXInd] <- normalGR[[sid]][chrXInd] - chrXMedian
+	values(normalGR)[[sid]][chrXInd] <- values(normalGR)[[sid]][chrXInd] - chrXMedian
 	
 }
 
-## compute median ##
-mat <- as.data.frame(normalGR)
-mat <- mat[, 11:ncol(mat)]
+
+mat <- values(normalGR)
 if (method == "median"){
    medianVal <- apply(mat, 1, median, na.rm = TRUE)
 }else if (method == "mean"){
@@ -99,7 +107,7 @@ if (method == "median"){
 }else{
   stop("method is not specified as median or mean.")
 }
-normalGR[["Median"]] <- medianVal
+values(normalGR)[["Median"]] <- medianVal
 
 ## output to text file ##
 write.table(as.data.frame(normalGR[,"Median"]), file=paste0(outfile, "_", method, ".txt"), col.names=TRUE, row.names=F, quote=F, sep="\t")
